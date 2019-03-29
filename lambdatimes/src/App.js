0e60ff44d5223567b467bb3b22f23344d58d6f16
joe-alfaro <josephlalfaro@gmail.com>
2019-03-29 09:14:35 -0700

    tab component reads and updates

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,14 +3,37 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
+import {tabData} from './data.js'
+import {cardData} from './data.js'
+import {carouselData} from './data.js'
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tabData: [],
+      cardData: [],
+      carouselData: []
+    };
+  };
+
+  componentDidMount(){
+    this.setState({
+      tabData: tabData,
+      cardData: cardData,
+      carouselData: carouselData
+    }); 
+  }
+
+  render(){
+    return (
+     <div className="App">
+       <TopBar />
+       <Header />
+       <Content tabData={this.state.tabData}/>
+     </div>
+    )
+  };
 }
 
 export default App;
